8959538698044826a2d57e0652816154e649ff0a
ej <evanrjohnso@gmail.com>
2017-12-04 20:46:39 -0800

    refactor

--- src/components/Square.js
+++ src/components/Square.js
@@ -4,9 +4,16 @@ import ReactDOM from 'react-dom';
 export default class Square extends React.Component {
     render() {
       return (
-        <button className="square">
-          {/* TODO */}
+        <button className="square" onClick={this.props.onClick()}>
+          {this.props.value}
         </button>
       );
     }
   }
+  // export function Square(props) {
+  //   return (
+  //     <button className="square" onClick={props.onClick}>
+  //       {props.value}
+  //     </button>
+  //   );
+  // }
